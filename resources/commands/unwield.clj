34832ae28825a16551c5aeb579deb239fee6e77a
Earle Ady <earle.ady@gmail.com>
2019-01-04 08:40:54 -0800

    wield/unwield

---
+++ resources/commands/unwield.clj
@@ -0,0 +1,27 @@
+(ns user
+  (:require [clojure.string :as str]
+            [mire.util :as util]
+            [mire.rooms :as rooms]
+            [mire.items :as items]
+            [mire.player :as player]))
+
+(defn unwield
+  "Unwield a weapon"
+  [args]
+  (if (> (count args) 0)
+    (let [thing (str/join " " args)]
+      ;; Is this thing in the players inventory?
+      (if (util/carrying? thing)
+        (let [id (util/find-item-in-ref player/*player* thing)
+              item (items/get-item id)]
+          ;; is this a weapon?
+          (if (items/wielded? item)
+            (do
+              (dosync
+                (alter items/items assoc-in [id :wielded] false))
+              (let [name (items/item-name (items/get-item id))]
+                (rooms/tell-others-in-room (str player/*name* " unwielded a " name "."))
+                (str "You stop wielding the " name ".")))
+            (str "You aren't wielding the " (items/item-name item) ".")))
+        (str "You're not carrying a " thing ".")))
+    (str "What do you want to unwield?")))
---
+++ resources/commands/wield.clj
@@ -0,0 +1,27 @@
+(ns user
+  (:require [clojure.string :as str]
+            [mire.util :as util]
+            [mire.rooms :as rooms]
+            [mire.items :as items]
+            [mire.player :as player]))
+
+(defn wield
+  "Wield a weapon"
+  [args]
+  (if (> (count args) 0)
+    (let [thing (str/join " " args)]
+      ;; Is this thing in the players inventory?
+      (if (util/carrying? thing)
+        (let [id (util/find-item-in-ref player/*player* thing)
+              item (items/get-item id)
+              name (items/item-name item)]
+          ;; is this a weapon?
+          (if (item :weapon)
+            (do
+              (dosync
+                (alter items/items assoc-in [id :wielded] true))
+              (rooms/tell-others-in-room (str player/*name* " wielded a " name "."))
+              (str "You wield the " name "."))
+            (str "You can't wield the " name ".")))
+        (str "You're not carrying a " thing ".")))
+    (str "What do you want to wield?")))
